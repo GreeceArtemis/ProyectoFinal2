d071a191fa109a181d1a71df5f4088608794aab0
GreeceArtemis <greece.rapu@gmail.com>
2017-03-06 15:53:37 -0500

    v1

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+function myFunction1() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+  var usuario=prompt("INGRESA TU NOMBRE: ");
+  document.getElementById("usuario").innerHTML="Hola coder "+usuario;
